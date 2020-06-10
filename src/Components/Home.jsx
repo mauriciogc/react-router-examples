51f30e0dba7af3a8e3a95817c7f27bd5c1c0d35a
mauriciogc <mauriciogc@gmail.com>
2020-06-10 11:04:16 -0500

    basic example: useHistory

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+const Home = () => {
+	const history = useHistory();
 
-const Home = () => <h1>Hello, I'm Home component</h1>;
+	const handleAbout = () => history.push("/about"); //add programmatic routing
+	const handleBack = () => history.goBack(); //Back programmatic routing
+	const handleForward = () => history.goForward(); //Forward programmatic routing
+
+	return (
+		<div>
+			<h1>Hello, I'm Home component </h1>
+			<button onClick={handleAbout}>About</button>
+			<button onClick={handleBack}>Back</button>
+			<button onClick={handleForward}>Forward</button>
+		</div>
+	);
+};
 
 export default Home;
