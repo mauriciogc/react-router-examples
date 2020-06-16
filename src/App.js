447a193823c666568d0e2b47e362139cd316cb03
mauriciogc <mauriciogc@gmail.com>
2020-06-16 10:44:40 -0500

    clean app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "./Components/Home";
-import Gallery from "./Components/Gallery";
-import About from "./Components/About";
 
 function App() { 
   return (
     <Fragment>
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Main page</Link>
-            </li>
-            <li>
-              <Link to="/gallery/5">Gallery</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </div>
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/gallery/:print">
-            <Gallery />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-      <footer> Hello I'm a little footer</footer>
+      App
     </Fragment>
   );
 }
